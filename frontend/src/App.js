3e84353d8ca3c5a066916d5e1945a4272c7a0473
Sourabh Kothari <sourabhkothari2014@gmail.com>
2023-07-23 20:08:22 +0530

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    balance: 0,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make a POST request to the Express backend to register the user
+      await axios.post('/api/register', formData);
+      console.log('Registration successful!');
+    } catch (error) {
+      console.error('Error registering user:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="balance">e₹ Balance</label>
+        <input
+          type="number"
+          id="balance"
+          name="balance"
+          value={formData.balance}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
